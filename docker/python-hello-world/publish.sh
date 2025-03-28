5f5dde94db096fac4e3e0e52268b88bf2cb2f9ae
mikelikesrobots <mikelikesrobots@outlook.com>
2025-03-28 10:06:14 +0000

    Detect region from ECR URI

--- docker/python-hello-world/publish.sh
+++ docker/python-hello-world/publish.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $ECR_REPO
+REGION=$(echo $ECR_REPO | cut -d'.' -f4)
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ECR_REPO
 docker push $ECR_REPO/python-hello-world:latest
